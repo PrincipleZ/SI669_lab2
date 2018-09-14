d9a01cd3b191d233e15ba201a57a1e3c00a16c5b
Yuanze Zhang <YuanzeZhang@0587366310.wireless.umich.net>
2018-09-14 13:26:27 -0400

    ec complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,7 +14,8 @@ export class HeroesComponent implements OnInit {
   };
   heroes = HEROES;
   selectedHero: Hero;
-
+  byAlpha = true;
+  buttonText = "Order Alphabetically";
 
   constructor() { }
 
@@ -25,5 +26,16 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero
   }
 
+  onClickSort(): void{
+    //Run sort here
+    if (this.byAlpha){
+        this.heroes.sort(function (a,b){return a.name[0] > b.name[0]});
+        this.buttonText = "Order By id";
+    } else {
+        this.heroes.sort(function (a,b){return a.id - b.id});
+        this.buttonText = "Order Alphabetically";
+    }
+    this.byAlpha = !this.byAlpha;
+  }
 }
 
